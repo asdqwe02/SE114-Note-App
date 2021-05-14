ebaaea1bf4fd8ffcee99d9d632cb60045c6dfd05
asdqwe02 <shaunakathien@gmail.com>
2021-05-14 21:25:47 +0700

    version 1 my asdqwe02-Branch

---
+++ NoteApp/app/src/main/java/vn/edu/uit/noteapp/Note_Screen_Bottom_Sheet_Setting.java
@@ -0,0 +1,4 @@
+package vn.edu.uit.noteapp;
+
+public class Note_Screen_Bottom_Sheet_Setting {
+}
---
+++ NoteApp/app/src/main/java/vn/edu/uit/noteapp/Note_screen.java
@@ -0,0 +1,13 @@
+package vn.edu.uit.noteapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Note_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_screen);
+    }
+}
