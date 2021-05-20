4af586cb0c29c5bb6e28a53f8d68cd481938f433
asdqwe02 <shaunakathien@gmail.com>
2021-05-20 08:34:18 +0700

    fixing overwrite error on merge

--- NoteApp/app/src/main/java/vn/edu/uit/noteapp/activity/Note_screen.java
+++ NoteApp/app/src/main/java/vn/edu/uit/noteapp/activity/Note_screen.java
@@ -4,14 +4,15 @@ import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
-
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -29,14 +30,15 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
-import vn.edu.uit.noteapp.adapter.Checkbox_recyclerview_adapter;
 import vn.edu.uit.noteapp.Checkbox_recyclerview_items;
-import vn.edu.uit.noteapp.bottomsheet.Note_Screen_Bottom_Sheet_Setting;
 import vn.edu.uit.noteapp.R;
+import vn.edu.uit.noteapp.adapter.Checkbox_recyclerview_adapter;
+import vn.edu.uit.noteapp.bottomsheet.Note_Screen_Bottom_Sheet_Setting;
 
-public class Note_screen extends AppCompatActivity implements Note_Screen_Bottom_Sheet_Setting.BottomSheetListener {
+public class Note_screen extends AppCompatActivity implements Note_Screen_Bottom_Sheet_Setting.BottomSheetListener  {
 
     private String note_screen_color;
+    private  boolean CRI_visibility=false;
 
     private static final String TITLE_FILE_NAME = "exampleTITLE.txt";
     private static final String NOTE_FILE_NAME = "exampleNOTE.txt";
@@ -44,6 +46,7 @@ public class Note_screen extends AppCompatActivity implements Note_Screen_Bottom
     //shared preferences
     public static final String SHARE_PREFS = "sharedPrefs";
     public static final String NOTE_SCREEN_COLOR = "note_screen_color";
+    public static final String CRI_VISIBILITY_STATE = "INVISIBLE DEFAULT";
 
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
@@ -52,6 +55,11 @@ public class Note_screen extends AppCompatActivity implements Note_Screen_Bottom
     EditText title_Text;
     EditText note_Text;
     ImageButton show_CheckBox;
+    Button Add_CRI_Btton;
+    EditText Add_CRI_Etext;
+    LinearLayout Add_CRI_Views;
+
+    public ArrayList<Checkbox_recyclerview_items> checkboxRecyclerviewItems= new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -66,32 +74,44 @@ public class Note_screen extends AppCompatActivity implements Note_Screen_Bottom
         note_Text=findViewById(R.id.noteText);
         show_CheckBox=findViewById(R.id.show_checkbox);
 
+        Add_CRI_Btton = findViewById(R.id.add_Checkbox_RecyclerView_items_Btton);
+        Add_CRI_Etext = findViewById(R.id.add_Checkbox_RecyclerView_items_Etext);
+        Add_CRI_Views = findViewById(R.id.add_CRI_views);
+
         show_CheckBox.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+
+                Sync_EditText_With_CheckBox_RecyclerView();
+
                 if (note_Text.getVisibility()==View.VISIBLE && mRecyclerView.getVisibility()==View.INVISIBLE){
                     note_Text.setVisibility(View.INVISIBLE);
                     mRecyclerView.setVisibility(View.VISIBLE);
+                    Add_CRI_Views.setVisibility(View.VISIBLE);
+
                 }
                 else {
                     note_Text.setVisibility(View.VISIBLE);
                     mRecyclerView.setVisibility(View.INVISIBLE);
+                    Add_CRI_Views.setVisibility(View.INVISIBLE);
                 }
 
             }
         });
 
-        //handling checkbox recyclerview
-        ArrayList<Checkbox_recyclerview_items> exampleList= new ArrayList<>();
-        exampleList.add(new Checkbox_recyclerview_items("text 1",false));
-        exampleList.add(new Checkbox_recyclerview_items("text 2",false));
-        exampleList.add(new Checkbox_recyclerview_items("text 3",false));
-        exampleList.add(new Checkbox_recyclerview_items("text 4",false));
+        Add_CRI_Btton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Add_CRI();
+            }
+        });
 
+        //handling checkbox recyclerview
         mRecyclerView = findViewById(R.id.checkbox_recyclerview_layout);
         mRecyclerView.setHasFixedSize(true); // if recycler view don't change set this to true note: will delete later
         mLayoutManager = new LinearLayoutManager(this);
-        mAdapter = new Checkbox_recyclerview_adapter(exampleList);
+        mAdapter = new Checkbox_recyclerview_adapter(checkboxRecyclerviewItems);
 
         mRecyclerView.setLayoutManager(mLayoutManager);
         mRecyclerView.setAdapter(mAdapter);
@@ -102,12 +122,43 @@ public class Note_screen extends AppCompatActivity implements Note_Screen_Bottom
         loadData();
         updateData();
     }
+
+    public void Sync_EditText_With_CheckBox_RecyclerView()
+    {
+        if (note_Text.getVisibility()==View.VISIBLE)
+            checkboxRecyclerviewItems.clear();
+        else {
+            note_Text.setText("");
+            for (int i = 0; i < checkboxRecyclerviewItems.size(); i++) {
+                //template: String title = ((TextView) recyclerView.findViewHolderForAdapterPosition(position).itemView.findViewById(R.id.title)).getText().toString();
+                String tempE= ((EditText) mRecyclerView.findViewHolderForAdapterPosition(i).itemView.findViewById(R.id.checkbox_edittext)).getText().toString();
+                note_Text.setText(note_Text.getText() + tempE + "\n");
+            }
+        }
+        String temp = note_Text.getText().toString();
+        String lines[] = temp.split("\\r?\\n");
+        for (int i=0;i<lines.length;i++)
+            checkboxRecyclerviewItems.add(new Checkbox_recyclerview_items(lines[i],false));
+        mLayoutManager = new LinearLayoutManager(this);
+        mAdapter = new Checkbox_recyclerview_adapter(checkboxRecyclerviewItems);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setAdapter(mAdapter);
+
+
+    }
+
+    public void Add_CRI(){
+        int position=checkboxRecyclerviewItems.size();
+        checkboxRecyclerviewItems.add(position,new Checkbox_recyclerview_items(Add_CRI_Etext.getText().toString(),false));
+        Add_CRI_Etext.setText("");
+        mAdapter.notifyItemInserted(position);
+    }
+
     public void Open_Bottom_Sheet_Setting()
     {
         Note_Screen_Bottom_Sheet_Setting bottomSheet = new Note_Screen_Bottom_Sheet_Setting();
         bottomSheet.show(getSupportFragmentManager(), "Note_Screen_bottomSheetSetting");
     }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
@@ -121,6 +172,7 @@ public class Note_screen extends AppCompatActivity implements Note_Screen_Bottom
                 Open_Bottom_Sheet_Setting();
                 return true;
             case android.R.id.home:
+                Sync_EditText_With_CheckBox_RecyclerView();
                 saveData();
                 finish();
                 return true;
@@ -139,6 +191,9 @@ public class Note_screen extends AppCompatActivity implements Note_Screen_Bottom
         SharedPreferences sharedPreferences = getSharedPreferences(SHARE_PREFS,MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
         editor.putString(NOTE_SCREEN_COLOR,hexColor);
+        if (mRecyclerView.getVisibility()==View.INVISIBLE)
+            editor.putBoolean(CRI_VISIBILITY_STATE,false);
+        else editor.putBoolean(CRI_VISIBILITY_STATE,true);
         editor.apply();
         saveNote();
     }
@@ -146,10 +201,23 @@ public class Note_screen extends AppCompatActivity implements Note_Screen_Bottom
     {
         SharedPreferences sharedPreferences = getSharedPreferences(SHARE_PREFS,MODE_PRIVATE);
         note_screen_color = sharedPreferences.getString(NOTE_SCREEN_COLOR,"#FFFFFF");
+        CRI_visibility=sharedPreferences.getBoolean(CRI_VISIBILITY_STATE,false);
+
     }
     public void updateData()
     {
         loadNote();
+        if (CRI_visibility)
+        {
+            Sync_EditText_With_CheckBox_RecyclerView();
+            mRecyclerView.setVisibility(View.VISIBLE);
+            Add_CRI_Views.setVisibility(View.VISIBLE);
+            note_Text.setVisibility(View.INVISIBLE);
+        }
+        else {
+            mRecyclerView.setVisibility(View.INVISIBLE);
+            Add_CRI_Views.setVisibility(View.INVISIBLE);
+        }
         View view = this.getWindow().getDecorView();
         view.setBackgroundColor(Color.parseColor(note_screen_color));
     }
@@ -282,6 +350,7 @@ public class Note_screen extends AppCompatActivity implements Note_Screen_Bottom
     @Override
     public void onBackPressed() {
         // your code.
+        Sync_EditText_With_CheckBox_RecyclerView();
         saveData();
         super.onBackPressed();
     }
